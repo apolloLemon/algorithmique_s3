72c15be904814f3c5a44ebd4c6ff98537012418a
Matthew Coyle <matthew@coyle.fr>
2018-10-02 09:56:44 +0200

    debut TD2~cours

---
+++ TD2/cours.cpp
@@ -0,0 +1,25 @@
+struct maillon {
+	int val;
+	maillon * suiv;
+}
+
+using Liste = maillon *;
+
+Liste L;
+L = Null;
+
+L = new maillon;
+(*L).val = 75;
+(*L).suiv = NULL;
+
+L->val;
+L->suiv;
+
+L->suiv->suiv->val;
+(*(*(*L).suiv).suiv).val;
+
+
+/*
+Pointeur 		type* varName
+valeur pointe' 	type *varName
+*/
