440423e86028630df508ffed883a9fead19d1edc
Matthew Coyle <matthew@coyle.fr>
2018-10-04 09:49:19 +0200

    InsertHead

--- TP2/exo01.cpp
+++ TP2/exo01.cpp
@@ -1,16 +1,42 @@
-struct maillon {
+#include <iostream>
+
+struct Link {
 	int val;
-	maillon * ante;
-	maillon * post;
-}
+	Link * ante;
+	Link * post;
+};
 
-using Liste = maillon *;
-
-int main () {}
+using List = Link *;
 
 //a
-void Init_Empty_List (Liste &L){
+void Init_Empty_List (List &L){
 	L = NULL;
 }
 
 //b
+bool Is_Empty (List L){
+	return (L==NULL);
+}
+
+//c
+void Insert_Head (List &L, int x){
+	List a = new Link;
+	a->val = x;
+	a->post = L;
+	if(L==NULL) a->ante = NULL;
+	else a->ante = L->ante;
+	L=a;
+}
+
+int main () {
+	List A;
+	Init_Empty_List(A);
+
+	std::cout << "Is the List Empty? "
+			<< (bool)Is_Empty(A)<<std::endl;
+
+	Insert_Head(A, 6);
+	Insert_Head(A, 2);
+	std::cout << "Is the List Empty? "
+			<< (bool)Is_Empty(A)<<std::endl;
+}
