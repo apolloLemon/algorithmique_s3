90ce7eb955fbad93d92dbc8d243d58a23193fe4a
Matthew Coyle <matthew@coyle.fr>
2018-10-09 10:03:03 +0200

    InsertTrie Cours

--- TD2/cours.cpp
+++ TD2/cours.cpp
@@ -23,3 +23,33 @@ L->suiv->suiv->val;
 Pointeur 		type* varName
 valeur pointe' 	type *varName
 */
+
+
+
+
+
+InserTrie (Liste L, int e) {
+	Liste nouveau = new maillon;
+	nouveau->val = e;
+	if(L==NULL){
+		nouveau->suiv=NULL;
+		L=nouveau;
+	} else {
+		Liste p = L;
+		if(p->val > e){
+			nouveau->suiv = p;
+			p = nouveau;
+		} else {
+			while ((p->suiv != NULL) && (p->val < e)){
+				p=p->suiv;
+				if(p->suiv == NULL){
+					p->suiv = nouveau;
+					nouveau->suiv = NULL;
+				} else {
+					nouveau->suiv = p->suiv;
+					p->suiv = nouveau;
+				}
+			}
+		}
+	}
+}
