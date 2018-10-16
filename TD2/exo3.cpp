0683710744f5249c6eec93ffc9622d3a2fe03d64
Matthew Coyle <matthew@coyle.fr>
2018-10-16 10:35:27 +0200

    TD2 exo 3 b et c

--- TD2/exo3.cpp
+++ TD2/exo3.cpp
@@ -24,15 +24,34 @@ bool one_LMent (Liste a){
 	return (a==a->next);
 }
 
-void affiche_Liste (Liste a) {
-	
+void affiche_Liste (const Liste &a) {}
+// const & optimise la copie
+//et protege le pointeur 
 
+void affiche_Liste (Liste a) {
+	if(Vide(a)) return;
 	a=a->next; //car la liste pointe vers le dernier
 
 	Liste premier = a;
 	do{
 		std::cout << a->val << std::endl;
 		a = a->next; 					//1
-	} while (a != premier);		//n
+	} while (a != premier);				//n
 }
 
+int length_iteratif (Liste a) {
+	int out = 0;
+	Liste b = a;
+	do {
+		out++;
+		a = a->next;
+	} while (a != b);
+
+	return out;
+}
+
+int length_recursif (Liste a, Liste b) {
+	if(a==NULL) return 0;
+	if(a==b) return 1;
+	return 1+length_recursif(a->next, b);
+}
