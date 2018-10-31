5bb14c87d56b8a7ecbffb04d31ed7957127ab277
Matthew Coyle <matthew@coyle.fr>
2018-10-31 10:37:04 +0100

    PostFix compiled

--- TP4/exo.cpp
+++ TP4/exo.cpp
@@ -8,48 +8,56 @@ const int TAILLE = 100;
 using Tab = std::array<float,TAILLE>;
 
 struct PILE {
-	Tab bob;
+	Tab val;
 	int som;
 };
 
+bool PileVide(PILE a);
 void Empiler(PILE &a, float x);
-void Depiler(PILE &a);
+float Depiler(PILE &a);
 
-float plus(float a, float b) {return a+b}
-float mult(float a, float b) {return a*b}
-float puis(float a, float b) {return pow(a,b)} //a^b
-float racc(float a) {return sqrt(a)}
+float plus(float a, float b) {return a+b;}
+float mult(float a, float b) {return a*b;}
+float puis(float a, float b) {return pow(a,b);} //a^b
+float racc(float a) {return sqrt(a);}
 
 int main(){
 	PILE A;
+	A.som=0;
 	std::istringstream expression("2 5 + 4 5 3 + 2 ^ x RAC x");
 
 	std::string s;
 
 	while(std::getline(expression, s, ' ')){
-		switch (s){
-			case '+':
-				Empiler(A,plus(Depiler(A),Depiler(A)));
-				break;
-			case '*':
-				Empiler(A,mult(Depiler(A),Depiler(A)));
-				break;
-			case '^':
-				Empiler(puis(Depiler(A),Depiler(A)));
-				break;
-			case "RAC":
-				Empiler(racc(Depiler(A)));
-				break;
-			default:
-				Empiler(A,std::stof(s));
-				break;
+		if(s=="+"){
+			Empiler(A,plus(Depiler(A),Depiler(A)));
+		} else if(s=="*"){
+			Empiler(A,mult(Depiler(A),Depiler(A)));
+		} else if(s=="^"){
+			Empiler(A,puis(Depiler(A),Depiler(A)));
+		} else if(s=="RAC"){
+			Empiler(A,racc(Depiler(A)));
+		} else {
+			Empiler(A,std::atof(s.c_str()));
 		}
 	}
+
+	std::cout<<A.val[0]<<std::endl;
+}
+
+bool PileVide(PILE a){
+	return (a.som);
+}
+
+bool PilePleine(PILE a){
+	return (a.som==100);
 }
 
 void Empiler(PILE &a, float x){
-
+	//if(!PilePleine(a))
+		a.val[a.som++]=x;
 }
-void Depiler(PILE &a){
-
+float Depiler(PILE &a){
+	//if(!PileVide(a))
+		return a.val[--a.som];
 }
