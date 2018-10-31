0feb4161dc463cda4ad62ab17032f11ea8198bc9
Matthew Coyle <matthew@coyle.fr>
2018-10-31 13:15:06 +0100

    TP4 correct result

--- TP4/exo.cpp
+++ TP4/exo.cpp
@@ -16,6 +16,8 @@ bool PileVide(PILE a);
 void Empiler(PILE &a, float x);
 float Depiler(PILE &a);
 
+void AffichePile(PILE a);
+
 float plus(float a, float b) {return a+b;}
 float mult(float a, float b) {return a*b;}
 float puis(float a, float b) {return pow(a,b);} //a^b
@@ -29,9 +31,10 @@ int main(){
 	std::string s;
 
 	while(std::getline(expression, s, ' ')){
+		std::cout << "getline " << s << std::endl;
 		if(s=="+"){
 			Empiler(A,plus(Depiler(A),Depiler(A)));
-		} else if(s=="*"){
+		} else if(s=="x"){
 			Empiler(A,mult(Depiler(A),Depiler(A)));
 		} else if(s=="^"){
 			Empiler(A,puis(Depiler(A),Depiler(A)));
@@ -40,9 +43,9 @@ int main(){
 		} else {
 			Empiler(A,std::atof(s.c_str()));
 		}
+		AffichePile(A);
 	}
 
-	std::cout<<A.val[0]<<std::endl;
 }
 
 bool PileVide(PILE a){
@@ -54,10 +57,21 @@ bool PilePleine(PILE a){
 }
 
 void Empiler(PILE &a, float x){
+	std::cout << "Empiler "<<x<<std::endl;
 	//if(!PilePleine(a))
 		a.val[a.som++]=x;
 }
 float Depiler(PILE &a){
+	std::cout << "Depiler "<<a.som<<std::endl;
 	//if(!PileVide(a))
-		return a.val[--a.som];
+		--a.som;
+		return a.val[a.som];
+}
+
+void AffichePile(PILE a){
+	std::cout << "Sommet : " << a.som<<std::endl;
+	for(int i=a.som-1;i>=0;i--){
+		std::cout<<a.val[i]<<" ";
+	}
+	std::cout<<std::endl;
 }
