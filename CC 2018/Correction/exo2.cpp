5e34cf02f136e4eb2d744b23bd9c793a9e91abdb
Matthew Coyle <matthew@coyle.fr>
2018-11-05 10:01:00 +0100

    Corr 2.3

--- CC 2018/Correction/exo2.cpp
+++ CC 2018/Correction/exo2.cpp
@@ -3,3 +3,14 @@ for(i=0;i<N;i++)
 	for(j=n-1;i<j;j--)
 		if(T[j]<T[j-1])
 			swap(T[j],T[j-1])
+
+//3
+bool b = true;
+while(i<N && b){
+	b = false;
+	for(j=n-1;i<j;j--)
+		if(T[j]<T[j-1]){
+			swap(T[j],T[j-1])
+			b=true;
+		}
+}
