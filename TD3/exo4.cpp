367901389bb5854f1088095cb75838c82979b364
Matthew Coyle <matthew@coyle.fr>
2018-11-05 10:50:33 +0100

    TD3 exo4 draft

--- TD3/exo4.cpp
+++ TD3/exo4.cpp
@@ -2,3 +2,18 @@ using liste = std::array<int,SIZE>;
 using matrix = std::array<liste,SIZE>;
 
 matrix adjacance;
+
+liste visites;
+int parcours_Proffondeur(matrix adj, int sommet, liste visites, int dist){
+	if(visites[s]==1) return;
+	visites[s] = 1;
+	for(int i=0;i<SIZE;i++){
+		if(s==i) continue;
+
+		if(adj[s][i]!=0){
+			cout << "De "<<s<<" a' "<<i<<" : "<<adj[s][i]<<" dist (Total: "<<dist+=adj[s][i]<<")\n";
+
+			parcours_Proffondeur(adj,i,visites, dist);
+		}
+	}
+}
