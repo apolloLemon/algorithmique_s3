c402df2006645375edc2bc816135a6da0d2599b0
Matthew Coyle <matthew@coyle.fr>
2018-11-08 10:09:02 +0100

    TD3.5

--- TD3/exo5ABC.cpp
+++ TD3/exo5ABC.cpp
@@ -1,4 +1,4 @@
-/*
+/* 1
 x= #allumette
 R(x) = #allumette apres qu'on ai joue'
 R(X) = X-1 or X-2 or X-3
@@ -20,3 +20,12 @@ si x not in K => x in { 4i+3, 4i+2, 4i+1}
 =>	R(4i+3) = {4i+2, 4i+1, 4i}
 or (4i)/4 = i in K
 => R(x) intersect K not = NULL
+*/
+
+/*2
+paper
+*/
+
+/*3
+Maintenir l'adversaire dans le noyau
+*/
