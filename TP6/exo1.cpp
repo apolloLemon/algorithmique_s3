da9332f387d09871b5cb8dbc28e1b95a54817866
Matthew Coyle <matthew@coyle.fr>
2018-11-15 10:39:00 +0100

    TP6.1.e-f

--- TP6/exo1.cpp
+++ TP6/exo1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 //a
 struct link {
@@ -14,32 +15,25 @@ struct roulette {
 
 //b
 void init_roulette (roulette &A){
-	//std::cout << "init_roulette"<<std::endl;
 	A.vals = nullptr;
 	A.taille = 0;
 }
 
 //c
 void ajoute_numero (roulette &A, int n) {
-	//std::cout << "ajoute_numero"<<std::endl;
 	A.taille++;
 	valsRoulette tmp = new link;
-	//std::cout << "ajoute_numero 1"<<std::endl;
 	tmp->val = n;
 
 	tmp->suiv = A.vals;
 	if(A.vals == NULL){
-	//std::cout << "ajoute_numero NULL"<<std::endl;		
 		A.vals = tmp;
 		tmp->suiv = tmp;
 	} else if (A.vals == A.vals->suiv){
-		//std::cout << "ajoute_numero uno"<<std::endl;
 		A.vals->suiv = tmp;
 	} else {
 		valsRoulette i = A.vals;
-		//std::cout << "ajoute_numero 3"<<std::endl;
 		while(i->suiv != A.vals) i = i->suiv;
-		//std::cout << "ajoute_numero 4"<<std::endl;
 		i->suiv = tmp;
 	}
 
@@ -50,7 +44,6 @@ void ajoute_numero (roulette &A, int n) {
 
 //d
 roulette creer_roulette (int n) {
-	//std::cout << "creer_roulette"<<std::endl;
 	roulette out;
 	init_roulette(out);
 
@@ -60,11 +53,29 @@ roulette creer_roulette (int n) {
 	return out;
 }
 
+//e
+void tourner_roulette (roulette &A){
+	A.vals = A.vals->suiv;
+}
+
+//f
+int lancer_roulette (roulette &A) {
+	int a = rand()%A.taille;
+	for(int i=0;i<a;i++){
+		tourner_roulette(A);
+	}
+	return A.vals->val;
+}
+
 int main () {
+	srand(time(NULL));
 
 	std::cout << "main"<<std::endl;
 	roulette a;
 	a = creer_roulette(5);
+
+	tourner_roulette(a);
+	tourner_roulette(a);
 
 	valsRoulette i = a.vals;
 	do {
@@ -72,6 +83,8 @@ int main () {
 		i = i->suiv;
 	} while(i != a.vals);
 	std::cout <<a.taille<<std::endl;
+	std::cout << std::endl;
+	std::cout <<lancer_roulette(a)<<std::endl;
 
 	std::cout <<"end"<<std::endl;
 	return 0;
