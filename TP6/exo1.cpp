6866fbed2e8e42d863d21513ca005b5141ad6cb5
Matthew Coyle <matthew@coyle.fr>
2018-11-15 09:33:11 +0100

    TP6.1.c

--- TP6/exo1.cpp
+++ TP6/exo1.cpp
@@ -15,3 +15,13 @@ void init_roulette (roulette &A){
 	A->vals = NULL;
 	A->taille = 0;
 }
+
+//c
+void ajoute_numero (roulette &A, int n) {
+	valsRoulette tmp = new link;
+	tmp->val = n;
+	tmp->suiv = A;
+	roulette i = A;
+	while(i->suiv != A) i = i->suiv;
+	i->suiv = tmp;	
+}
