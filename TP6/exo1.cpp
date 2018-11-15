1e19d48ca89fe8162a561a188430aa33c2ea2aec
Matthew Coyle <matthew@coyle.fr>
2018-11-15 10:17:30 +0100

    TP6.1.b changed NULL to nullptr to please the c++11 compiler
    TP6.1.c
    fixed ajoute_numero

--- TP6/exo1.cpp
+++ TP6/exo1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 //a
 struct link {
 	int val;
@@ -12,34 +14,65 @@ struct roulette {
 
 //b
 void init_roulette (roulette &A){
-	A->vals = NULL;
-	A->taille = 0;
+	//std::cout << "init_roulette"<<std::endl;
+	A.vals = nullptr;
+	A.taille = 0;
 }
 
 //c
 void ajoute_numero (roulette &A, int n) {
+	//std::cout << "ajoute_numero"<<std::endl;
 	A.taille++;
 	valsRoulette tmp = new link;
+	//std::cout << "ajoute_numero 1"<<std::endl;
 	tmp->val = n;
+
 	tmp->suiv = A.vals;
-	valsRoulette i = A.vals;
-	while(i->suiv != A.vals) i = i->suiv;
-	i->suiv = tmp;	
+	if(A.vals == NULL){
+	//std::cout << "ajoute_numero NULL"<<std::endl;		
+		A.vals = tmp;
+		tmp->suiv = tmp;
+	} else if (A.vals == A.vals->suiv){
+		//std::cout << "ajoute_numero uno"<<std::endl;
+		A.vals->suiv = tmp;
+	} else {
+		valsRoulette i = A.vals;
+		//std::cout << "ajoute_numero 3"<<std::endl;
+		while(i->suiv != A.vals) i = i->suiv;
+		//std::cout << "ajoute_numero 4"<<std::endl;
+		i->suiv = tmp;
+	}
+
+	tmp->suiv = A.vals;
+
+		
 }
 
 //d
 roulette creer_roulette (int n) {
+	//std::cout << "creer_roulette"<<std::endl;
 	roulette out;
 	init_roulette(out);
 
 	for(int i=0;i<n;i++){
 		ajoute_numero(out, i);
 	}
+	return out;
 }
 
 int main () {
 
+	std::cout << "main"<<std::endl;
+	roulette a;
+	a = creer_roulette(5);
 
+	valsRoulette i = a.vals;
+	do {
+		std::cout << i->val << std::endl;
+		i = i->suiv;
+	} while(i != a.vals);
+	std::cout <<a.taille<<std::endl;
 
+	std::cout <<"end"<<std::endl;
 	return 0;
 }
