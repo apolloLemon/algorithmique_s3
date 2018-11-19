9ac5edd228aaf61b2121f1630bcc507ead3a7304
Matthew Coyle <matthew@coyle.fr>
2018-11-19 10:05:56 +0100

    TD4.C.2

---
+++ TD4/C/exo2.cpp
@@ -0,0 +1,37 @@
+struct knot {
+	char a;
+	knot * g;
+	knot * d;
+};
+using tree = knot *;
+
+const int TAILLE = 100;
+using Tab = std::array<tree,TAILLE>;
+
+struct PILE {
+	Tab val;
+	int som;
+};
+
+bool PileVide(PILE a);
+void Empiler(PILE &a, int x);
+int Depiler(PILE &a);
+void AffichePile(PILE a);
+
+void affiche_Iteratif_with_help (tree A) {
+	if(A==NULL) return;
+
+	PILE P; P.som = 0;
+	tree current;
+	Empiler(P,A);
+	while(!PileVide(P)){
+		current = Depiler(P);
+		std::cout<<current->val<<" ";
+		if(current->d!=NULL){
+			Empiler(P,current->d)
+		}
+		if(current->g!=NULL){
+			Empiler(P,current->g)
+		}
+	}
+}
