c5819832fc62eab38f75c4cd4b90d87f9fae0323
Matthew Coyle <matthew@coyle.fr>
2018-11-26 22:56:39 +0100

    TD4.F addtoSortedList ++ main

--- TD4/F.cpp
+++ TD4/F.cpp
@@ -1,3 +1,8 @@
+#include <array>
+#include <vector>
+#include <utility>
+#include <iostream>
+
 /*a
 le minimum dans un ABPPO est a' la racine de l'arbre, 
 et c'est le premier du arr RPZ l'ABPPO
@@ -11,7 +16,7 @@ struct ABPPO {
 	int taille;
 };
 
-//b
+//b O(log2 n)
 void srtLM (ABPPO &a, int i){
 	//if(i%2!=0) i--;
 	if(i<1)return;
@@ -31,20 +36,21 @@ void ajouteLM (ABPPO &a, int x){
 	srtLM(a,i);
 }
 
-//c
+//c O(log2 n)
 void SrtABPPO (ABPPO &a, int i){
-	if(i>taille) return;
+	int s;
+	if(i>a.taille) return;
 	if(a.tas[i] > a.tas[i*2] 
 		or a.tas[i] > a.tas[i*2+1])	{
-		int s = (int)(a.tas[i*2] >= a.tas[i*2+1]);
-		swap(a.tas[i],a.tas[2*i+s]);
+		s = (int)(a.tas[i*2] >= a.tas[i*2+1]);
+		std::swap(a.tas[i],a.tas[2*i+s]);
 	}
 	SrtABPPO(a,2*i+s);
 }
 
 int rmMin (ABPPO &a){
 	int out = a.tas[1];
-	swap(a.tas[1],a.tas[a.taille--]);
+	std::swap(a.tas[1],a.tas[a.taille--]);
 	SrtABPPO(a,1);
 	return out;
 }
@@ -56,17 +62,44 @@ HEAP SORT
 //pre-A
 
 
-//a
-void List2Tree (ABPPO &a, list b){
+//a O(n log2 n)
+void List2Tree (ABPPO &a, std::vector<int> b){
 	for(int i=0;i<b.size();i++){
-		ajouteLM(a,b[i]);
+		ajouteLM(a,b[i]); //O(log2 n)
 	}
 }
 
 //b
 //void initList (liste &b){}
 
-//c&d
-void addtoSortedList (ABPPO &a, list &b){
+//c&d O(n log2 n)
+void addtoSortedList (ABPPO &a, std::vector<int> &b){
+	int i=0;
+	while(a.taille>0){
+		b.push_back(rmMin(a));
+	}
+}
 
+int main () {
+
+	std::vector<int> b = {16,14,12,15,123,19,6,2,7,4,9844,3214,7779};
+	std::vector<int> v;
+	//v.size=0;
+	ABPPO a;
+
+	for(int i=0;i<b.size();i++){
+		std::cout<<b[i]<<std::endl;
+	}
+
+	std::cout<<"List2Tree\n";
+	List2Tree(a,b);
+	
+	std::cout<<"addtoSortedList\n";
+	addtoSortedList(a,v);
+
+	for(int i=0;i<v.size();i++){
+		std::cout<<v[i]<<std::endl;
+	}
+
+	return 0;
 }
