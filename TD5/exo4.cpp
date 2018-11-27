fcda198f3ead6feb7301602eae3298d67abd0ea3
Matthew Coyle <matthew@coyle.fr>
2018-11-27 10:51:27 +0100

    TD5.3-4

---
+++ TD5/exo4.cpp
@@ -0,0 +1,18 @@
+int pluspetit (ABR A){
+	while(A->g!=NULL){
+		A=A->g;
+	}
+	return A->val;
+}
+
+int plusgrand (ABR A){
+	while(A->d!=NULL){
+		A=A->d;
+	}
+	return A->val;
+}
+
+bool ppdom(ABR A, ABR B) {
+	return (pluspetit(A)>=pluspetit(B) 
+		and plusgrand(A)<=plusgrand(B));
+}
