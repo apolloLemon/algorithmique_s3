0fc044b17f3f5a2a344734d27676e4038ae4019e
Matthew Coyle <matthew@coyle.fr>
2018-11-29 09:40:36 +0100

    TP7.1.2

--- TP7/exo1.cpp
+++ TP7/exo1.cpp
@@ -4,6 +4,25 @@ struct knot {
 	knot * g;
 	knot * d;
 };
-using ArbreBinaire = knot *;
+using BiTree = knot *;
 
 //b
+void ajouteFeuille (BiTree &a, BiTree &b) {
+	if(b.x > a.x){
+		if(a->d!=NULL) ajouteFeuille(a->d,b);
+		else a.d=b;
+
+	} else { 
+	
+		if(a->g!=NULL) ajouteFeuille(a->g,b);
+		else a.g=b;
+	}
+}
+
+void ajouteFeuille (BiTree &a, int e) {
+	BiTree b = new knot;
+	b.x = e;
+
+	if(a==NULL) a = b;
+	else ajouteFeuille(a,b);	
+}
