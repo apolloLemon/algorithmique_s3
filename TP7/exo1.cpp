f05c79da378704d2f3e1c2125f65547c3b867b46
Matthew Coyle <matthew@coyle.fr>
2018-11-29 10:07:59 +0100

    TD7.1.b-c fixed . to -> problems tested b-c

--- TP7/exo1.cpp
+++ TP7/exo1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 //a
 struct knot {
 	int x;
@@ -8,21 +10,51 @@ using BiTree = knot *;
 
 //b
 void ajouteFeuille (BiTree &a, BiTree &b) {
-	if(b.x > a.x){
-		if(a->d!=NULL) ajouteFeuille(a->d,b);
-		else a.d=b;
+	if(b->x > a->x){
+		if(a->d!=nullptr) ajouteFeuille(a->d,b);
+		else a->d=b;
 
 	} else { 
 	
-		if(a->g!=NULL) ajouteFeuille(a->g,b);
-		else a.g=b;
+		if(a->g!=nullptr) ajouteFeuille(a->g,b);
+		else a->g=b;
 	}
 }
 
 void ajouteFeuille (BiTree &a, int e) {
 	BiTree b = new knot;
-	b.x = e;
+	b->x = e;
 
-	if(a==NULL) a = b;
+	if(a==nullptr) a = b;
 	else ajouteFeuille(a,b);	
+}
+
+//c
+void afficheNoeud (BiTree a){
+	std::cout <<"@ "<< a << "\te = " <<a->x
+		<<"\t\tg@ "<<a->g<<"\td@ "<<a->d<<std::endl;
+}
+
+void afficheBiTree (BiTree a) {
+	if(a->g!=nullptr) afficheBiTree(a->g);
+	if( a  !=nullptr) afficheNoeud(a);
+	if(a->d!=nullptr) afficheBiTree(a->d);
+}
+
+
+int main (){
+	BiTree a;
+	a=nullptr;
+	ajouteFeuille(a,2);
+	ajouteFeuille(a,1);
+	ajouteFeuille(a,8);
+	ajouteFeuille(a,94);
+	ajouteFeuille(a,33);
+	ajouteFeuille(a,4);
+	ajouteFeuille(a,9);
+	ajouteFeuille(a,-1);
+	ajouteFeuille(a,-60);
+
+	afficheBiTree(a);
+
 }
