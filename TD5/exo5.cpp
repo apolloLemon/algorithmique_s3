1c60ba632cd3cc47456b3ae092faff942fd5ea43
Matthew Coyle <matthew@coyle.fr>
2018-12-03 16:26:08 +0100

    TD5.5

---
+++ TD5/exo5.cpp
@@ -0,0 +1,32 @@
+/*
+d(a)=h(a->g)-h(a->d)
+if(d(a)) = {-1,0,1} => equilibre'
+
+if(d(a)) == -2 => right is heavier
+d(a->d) == -1 => rg(a) will balance the tree
+d(a->d) == 1 => rgd(a)
+
+d(a) == 2 => left is heavier
+d(a->g) == 1 => rg(a)
+d(a->g) == -1 => rdg(a)
+
+*/
+
+
+//teacher code
+void ajout (tree &a, int x){
+	while(!feuille(a)){
+		if(x >= a->val) {
+			empiler(a->d, P)
+			while(!vide(P)){
+				t = depile(P)
+				if (d(t)=2 or -2) {
+					cherche_bonne_rotation(t);
+				}
+			}
+		}
+		else { //(x < a->val)
+			//opposite
+		}
+	}
+}
