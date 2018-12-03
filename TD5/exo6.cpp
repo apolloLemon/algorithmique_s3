9f053396a125078f729463c5bc8799afef0e782e
Matthew Coyle <matthew@coyle.fr>
2018-12-03 10:19:21 +0100

    TD5 left and right rotations

---
+++ TD5/exo6.cpp
@@ -0,0 +1,28 @@
+struct noeud {
+	int e;
+	noeud * g;
+	noeud * d;
+};
+using tree = noeud *;
+
+void rotation_gauche (tree &a){
+
+	tree p = a;
+	a = a->d;
+	p->d = a->g;
+	a->g = p;
+
+	//tree i = a->g;
+	//while(i->g!=nullptr) i=i->g; wrong
+	//i->g = tmp;
+
+}
+
+void rotation_droite (tree &a) {
+
+	tree p = a;
+	a = a->g;
+	p->g = a->d;
+	a->d = p;
+
+}
