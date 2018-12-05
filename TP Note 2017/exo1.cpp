fd273ef38d8515bd7becdb31f0c7ddb41cc1615f
Matthew Coyle <matthew@coyle.fr>
2018-12-05 20:49:28 +0100

    TPNote' 1.3-4

--- TP Note 2017/exo1.cpp
+++ TP Note 2017/exo1.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 //1
 struct knot {
 	std::string str;
@@ -12,4 +14,24 @@ void initArbre (AB &a){
 }
 
 //3
+void addKnot_to (AB &a, std::string etiq){
+	a = new knot;
+	a->str = etiq;
+	a->g=a->d=nullptr;
+}
 
+void addKnot (AB &a, char s, std::string etiq){
+	if(s=='G'){
+		addKnot_to(a->g, etiq);
+	} else 
+	if(s=='D'){
+		addKnot_to(a->d, etiq);	
+	}
+}
+
+//4
+bool est_feuille (AB a) {
+	return (!a->g) and (!a->d);
+}
+
+int main (){return 0;}
