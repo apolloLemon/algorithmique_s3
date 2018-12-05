8761ffdae5522e3ceaa8876f3235bdcfc4ea335c
Matthew Coyle <matthew@coyle.fr>
2018-12-05 21:00:45 +0100

    TP note' 1.5-6

--- TP Note 2017/exo1.cpp
+++ TP Note 2017/exo1.cpp
@@ -34,4 +34,20 @@ bool est_feuille (AB a) {
 	return (!a->g) and (!a->d);
 }
 
+//5
+bool est_interne (AB a) {
+	return !est_feuille(a);
+}
+
+//6
+AB addressOf (AB a, std::string etiq) {
+	if(a==nullptr) return NULL;
+	if(a->str == etiq) return a;
+	AB g = addressOf(a->g, etiq);
+	AB d = addressOf(a->g, etiq);
+	if(g) return g;
+	else return d;
+}
+
+
 int main (){return 0;}
